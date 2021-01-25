e43a561fb771c9ff061e014b9aa35f1753e28776
root <root@DESKTOP-ISOVIOO.localdomain>
2021-01-25 14:22:09 +0100

    double numbers fixed

--- lotto.js
+++ lotto.js
@@ -5,14 +5,20 @@ function myLottery() {
 
     for (i = 0; lottoArray.length < 5; i++) {
         let randomNumber = Math.floor(Math.random() * 49 + 1);
-        lottoArray.splice(i, 0, randomNumber);
+        if (!lottoArray.includes(randomNumber)) {
+            lottoArray.splice(i, 0, randomNumber);
+            console.log("did this happen", lottoArray);
+        }
+
     }
 
     let euroZahlen = [];
 
     for (i = 0; euroZahlen.length < 2; i++) {
         let randomNumber = Math.floor(Math.random() * 10 + 1);
-        euroZahlen.splice(i, 0, randomNumber);
+        if (!euroZahlen.includes(randomNumber)) {
+            euroZahlen.splice(i, 0, randomNumber);
+        }
     }
 
     //console.log(lottoArray);
